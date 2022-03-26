4361275bdc835929af1e11a5a3ddabd08e28d3b1
Wa4e7o <70140902+Wa4e7o@users.noreply.github.com>
2022-03-26 12:36:36 +0200

    Visualize Actiors

--- MovieSystem/Controllers/ProducersController.cs
+++ MovieSystem/Controllers/ProducersController.cs
@@ -1,8 +1,9 @@
 ﻿namespace MovieSystem.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
     using MovieSystem.Data;
-    using System.Linq;
+    using System.Threading.Tasks;
 
     public class ProducersController : Controller
     {
@@ -13,10 +14,10 @@
             this.data = data;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var baseData = data.Producers.ToList();
-            return View(baseData);
+            var allProducers = await data.Producers.ToListAsync();
+            return View(allProducers);
         }
     }
 }
--- MovieSystem/Data/Models/Actior.cs
+++ MovieSystem/Data/Models/Actior.cs
@@ -10,14 +10,17 @@
         [Key]
         public int Id { get; init; }
 
+        [Display(Name = "Full Name")]
         [Required]
         [MaxLength(NameMaxLength)]
         public string Name { get; set; }
 
+        [Display(Name = "Biography")]
         [Required]
         [MaxLength(BiographyMaxLength)]
         public string Biography { get; set; }
 
+        [Display(Name="Profil Picture Url")]
         public string ImageURL { get; set; }
 
         public List<ConnectionTable> ConnectionTables { get; set; }
